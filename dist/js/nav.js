c6f69677e042a7a3710eca6cfec8c598497b8533
Adam Jolicoeur <adam.jolicoeur@gmail.com>
2016-11-15 14:51:16 -0500

    Add distribution directory for GitHub pages

---
+++ dist/js/nav.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#includeNavigation").load("includes/_nav.html");
+    $("#includeFooter").load("includes/footer.html");
+    $("#includeFilters").load("includes/_filters.html"); 
+});
