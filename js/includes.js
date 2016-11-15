9c6fb564eb9619ac8bc22336d6cdf2064548f529
Adam Jolicoeur <adam.jolicoeur@gmail.com>
2016-11-15 14:24:19 -0500

    Basic Dashboard, Planner and Board views

---
+++ js/includes.js
@@ -0,0 +1,15 @@
+$(function(){
+    $("#includeNavigation").load("includes/nav.html");
+    $("#includeFilters").load("includes/filters.html"); 
+
+    // Dashboard
+    $("#includeDashboard").load("includes/intro.html");
+
+    // Plan View
+    $("#includePlanView").load("includes/work/plan-view.html");
+
+    // Agile Card View
+    $("#includeAgileCards").load("includes/work/board-view.html");
+
+    $("#includeFooter").load("includes/footer.html");
+});
