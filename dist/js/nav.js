92b9207bd08eb29887be66b571b4672f1f57c201
Adam Jolicoeur <adam.jolicoeur@gmail.com>
2016-11-16 14:08:03 -0500

    remove dist folder

--- dist/js/nav.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    $("#includeNavigation").load("includes/_nav.html");
-    $("#includeFooter").load("includes/footer.html");
-    $("#includeFilters").load("includes/_filters.html"); 
-});
