dd8e5e8b409f6a7fddc625410de72670d0b33a73
Adam Jolicoeur <adam.jolicoeur@gmail.com>
2017-02-04 12:39:55 +0100

    add hide/show ability to panel

--- js/includes.js
+++ js/includes.js
@@ -1,29 +1,29 @@
 $(function(){
-    $("#includeNavigation").load("includes/nav.html");
-    $("#includeFilters").load("includes/filters.html");
+  $("#includeNavigation").load("includes/nav.html");
+  $("#includeFilters").load("includes/filters.html");
 
-    // Dashboard
-    $("#includeDashboard").load("includes/dashboard.html");
+  // Dashboard
+  $("#includeDashboard").load("includes/dashboard.html");
 
-    // Plan View
-    $("#includeIterations").load("includes/work/iterations.html");
-    $("#includePlanView").load("includes/work/plan.html");
-    $("#includeDetailPanel").load("includes/work/detail-panel.html");
+  // Plan View
+  $("#includeIterations").load("includes/work/iterations.html");
+  $("#includePlanView").load("includes/work/plan.html");
+  $("#includeDetailPanel").load("includes/work/detail-panel.html");
 
-    // Board View
-    $("#includeAgileCards").load("includes/work/board.html");
+  // Board View
+  $("#includeAgileCards").load("includes/work/board.html");
 
-    $("#includeAddFooter").load("includes/add-footer.html");
+  $("#includeAddFooter").load("includes/add-footer.html");
 
-    $("#includeFooter").load("includes/footer.html");
+  $("#includeFooter").load("includes/footer.html");
 
-    $("#includeWarning").load("includes/warning.html");
+  $("#includeWarning").load("includes/warning.html");
 
-    // Codebases
-    $("#includeCodebase").load("includes/create/codebase.html");
-    // Workspaces
-    $("#includeWorkspace").load("includes/create/workspace.html");
+  // Codebases
+  $("#includeCodebase").load("includes/create/codebase.html");
+  // Workspaces
+  $("#includeWorkspace").load("includes/create/workspace.html");
 
-    // Toolbar
-    $("#includeToolbar").load("includes/toolbar.html");
+  // Toolbar
+  $("#includeToolbar").load("includes/toolbar.html");
 });
